086e937c052a0947f32d898cfbceb9aee22a1911
Anubhav311 <anubhavbhambri1@gmail.com>
2019-10-12 22:36:37 +0530

    completed recipe_batches.py

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,25 @@
 
 import math
 
+# find items in recipe dictionary => 10 units of milk
+# find items in ingredients dictionary => 20 units of milk
+# divide ingredients by recipe (20/10 = 2)
+
 def recipe_batches(recipe, ingredients):
-  pass 
+  counter = math.inf
+
+  for key in recipe:
+    if ingredients.get(key) and ingredients[key] / recipe[key] >= 1:
+      counter = min(counter, ingredients[key] // recipe[key])
+    else:
+      return 0
+
+  return counter
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+# if __name__ == '__main__':
+#   # Change the entries of these dictionaries to test 
+#   # your implementation with different inputs
+#   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+#   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+#   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
